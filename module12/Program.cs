0557645a71a61346656e2eaa387a866565e4223f
Eugene294 <koblia.eo@gmail.com>
2024-07-07 18:29:35 +1000

    Добавьте файлы проекта.

---
+++ module12/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace module12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User("User", "Name", false);
+
+            Console.WriteLine("Работа непремиум-пользователя");
+            user.Action();
+            Console.WriteLine("Работа премиум-пользователя");
+            user.BuyPremium();
+            user.Action();
+
+        }
+
+    }
+}
---
+++ module12/User.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace module12
+{
+    internal class User
+    {
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public bool IsPremium { get; set; }
+
+        public User(string Login, string Name, bool IsPremium)
+        {
+            this.Login = Login;
+            this.Name = Name;
+            this.IsPremium = IsPremium;
+        }
+
+        public void Action()
+        {
+            if (!IsPremium)
+            {
+                ShowAds();
+            }
+            Console.WriteLine("Действие 1 выполнено");
+            Thread.Sleep(1500);
+            Console.WriteLine("Действие 2 выполнено");
+            Thread.Sleep(1500);
+            Console.WriteLine("Действие 3 выполнено");
+            Thread.Sleep(1500);
+        }
+
+        public void BuyPremium ()
+        {
+            IsPremium = true;
+            Console.WriteLine("Премиум-подписка куплена");
+        }
+
+        public void ShowAds()
+        {
+                Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+                // Остановка на 1 с
+                Thread.Sleep(1000);
+
+                Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+                // Остановка на 2 с
+                Thread.Sleep(2000);
+
+                Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+                // Остановка на 3 с
+                Thread.Sleep(3000);
+            
+        }
+    }
+}
